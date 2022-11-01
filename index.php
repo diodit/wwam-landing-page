b6240a12cfa33ac227d9fa43e6dc8b6e9e496668
diodit <dominicdamien@yahoo.co.uk>
2022-11-01 15:01:13 -0700

    WWAM landing page

---
+++ index.php
@@ -0,0 +1,386 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all">
+        <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700" rel="stylesheet">
+    
+    <style>
+
+.signup-for-wwam {
+  width: 758px;
+  height: 22px;
+  margin: 72px 3px 0 0;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.35;
+  letter-spacing: normal;
+  text-align: left;
+  color: #0cb5dd;
+}
+.signup-for-wwam .text-style-1{
+  color: #212121;
+}
+
+.meet-content-text{
+  width: 761px;
+  height: 261px;
+  margin: 0 0 32px;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.3;
+  letter-spacing: normal;
+  text-align: left;
+  color: #212121;
+  text-align: justify;
+}
+
+
+.meet-content-textt {
+  width: 758px;
+  height: 106px;
+  margin: 32px 3px 72px 0;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.3;
+  letter-spacing: normal;
+  text-align: left;
+  color: #212121;
+
+}
+.built-with-love {
+  width: 227px;
+  height: 16px;
+  margin: 0 0 13px;
+  font-family: HelveticaNeue;
+  font-size: 13px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 2.46;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+}
+.built-with-love .text-style-1 {
+  font-weight: bold;
+}
+
+
+.business-blog-jobs{
+  width: 222px;
+  height: 19px;
+  margin: 0 12px 35px 12px;
+  font-family: HelveticaNeue;
+  font-size: 16px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+
+}
+.all-right-reserved{
+    width: 315px;
+  height: 19px;
+  margin: 34px 0 0;
+  font-family: HelveticaNeue;
+  font-size: 16px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+}
+.soon-join {
+  width: 433px;
+  height: 49px;
+  padding-top:24px;
+  margin: 24px 14px 40px 0px;
+  opacity: 0.4;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #111827;
+}
+
+
+.whereweallmeet {
+  width: 358px;
+  height: 53px;
+  margin: 8px 89px 24px 0px;
+  font-family: HelveticaNeue;
+  font-size: 44px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.68;
+  letter-spacing: normal;
+  text-align: left;
+  color: #32b0cf;
+}
+.whereweallmeet .text-style-1 {
+  color: #ca1c67;
+}
+.whereweallmeet .text-style-2 {
+  color: #52bd90;
+}
+.whereweallmeet .text-style-3 {
+  color: #f3b825;
+}
+
+
+.dating-future{
+
+    
+  width: 362px;
+  height: 38px;
+  margin: 0 85px 8px 0px;
+  font-family: HelveticaNeue;
+  font-size: 32px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.94;
+  letter-spacing: normal;
+  text-align: left;
+  color: #0cb5dd;
+
+}
+.Signup-to-become-a-WhereWeAllMeetcom-dating-agent-and-get-paid-to-refer-your-social-connections-to {
+  width: 518px;
+  height: 179px;
+  margin: 32px 173px 2px 0;
+  font-family: HelveticaNeue;
+  font-size: 24px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+}
+.Signup-to-become-a-WhereWeAllMeetcom-dating-agent-and-get-paid-to-refer-your-social-connections-to .text-style-1 {
+  font-weight: bold;
+  color: #0cb5dd;
+}
+
+
+.Are-you-a-social-influencer-with-a-large-personal-social-network {
+  width: 409px;
+  height: 230px;
+  margin: 35px 282px 32px 0;
+  font-family: HelveticaNeue;
+  font-size: 52px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.08;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+}     
+
+img.influencer-img {
+  width: 478px;
+  height: 478px;
+  margin: 0 0 0 173px;
+  object-fit: contain;
+}
+
+    
+    .content-text{
+        width: 761px;
+  height: 261px;
+  margin: 0 0 32px;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.3;
+  letter-spacing: normal;
+  text-align: left;
+  color: #212121;
+    }
+       p .Sign-up-for-the-WhereWeAllMeet-private-beta {
+  width: 758px;
+  height: 22px;
+  margin: 72px 3px 0 0;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.35;
+  letter-spacing: normal;
+  text-align: left;
+  color: #0cb5dd;
+}
+.Sign-up-for-the-WhereWeAllMeet-private-beta .text-style-1 {
+  color: #212121;
+}
+    </style>
+    </head>
+    <body data-smooth-scroll-offset="77">
+        <div class="nav-container">
+            <div>
+                <nav class="bar bar-toggle">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-6">
+                                <div class="bar__module">
+
+                                        <a href="index.php"> <img class="logo logo-dark" src="img/nav-wwam-logo.png" srcset="img/nav-wwam-logo@2x.png 2x, img/nav-wwam-logo@3x.png 3x"></a>
+                                   
+                                </div>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end">
+                                <div class="bar__module">
+                                 
+
+                                    <!-- <div class="bar__module">
+                                        <a  href="#customise-template"> <span  style="margin-left:-200px;">
+                                        Jobs Blog
+                                    </span> </a>
+                                     
+                                    </div> -->
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+ 
+      
+           
+            <section class="text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-lg-6">
+                            
+                            <img src="img/image.png" srcset="img/image@2x.png 2x, img/image@3x.png 3x" class="image" style=" width: 510px;
+                            height: 444.8px;
+                        "> 
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+             
+            <!-- <section class="text-center" data-overlay="4">
+                <div class="background-image-holder"> <img alt="background" src="img/bg.png"> </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-10 col-lg-8">
+
+                            <img src="img/influencer-img.png" srcset="img/influencer-img@2x.png 2x, img/influencer-img@3x.png 3x" class="image"> 
+                          
+                        </div>
+                    </div>
+                </div>
+            </section> -->
+
+            <section class="text-center" data-overlay="4" style="margin-top:-90px;">
+                <div class="background-image-holder"> <img alt="background" src="img/bgg_new.png"> </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-10 col-lg-8">
+                           <p class="lead" style="color:black; font-family: HelveticaNeue; font-size: 20px; margin-top:120px;  " > Meeting New People should be interactive. Not based solely on algorithms and images, but mirror the same process of human interaction. We all have friends and would like to meet new friends. Our inner circle of friends usually represents our interests, our values and our attractions. WhereWeAllMeet.com is the new way of dating and meeting new people. We have created a community that allows friends to gather, discuss things that they have in common, attend local events, and expand their friends network by meeting new friends of friends. We believe the best way to meet new people is within a network of people that share your interests and want to meet. Join us in our community of people looking to meet people for Socializing, Friendship, and Dating. </p>
+                                 
+                                  <span class="meet-content-text">
+                                    Tired of searching for hours online for someone that is interested in connecting with you? WhereWeAllMeet.com’s dating agents are people with a large personal social network that members can subscribe and get introduced to potential dates that match what you’re looking for in your life
+                                  </span>
+                                  
+
+                                
+                        
+                                      <p class="signup-for-wwam">
+                                        <span class="text-style-1">Sign up for the</span>
+                                       <a href="wwam-private-beta.php" style="text-decoration:none; color: #0cb5dd;"> WhereWeAllMeet private beta<span class="text-style-2">.</a></span>
+                                      </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+           
+
+
+        
+          
+
+
+           
+            <footer class="text-center space--sm footer-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+
+
+
+                            <div> 
+                                
+                                    <span class="built-with-love">
+                                       <b> Built with
+                                        <span class="text-style-1">♡</span>
+                                        by friends of wwam.
+                                        RSS</b>
+                                    </span>
+                            </div>
+
+                            <div>
+                             
+                                <ul class="social-list list-inline list--hover" >
+                                    <li><a href="#"> <div style="height:20px; width:20px; background-color: black; border-color:black;"></div></a></li>
+                                    <li><a href="#"> <div style="height:20px; width:20px; background-color: black; border-color:black;"></div></a></li>
+                                    <li><a href="#"> <div style="height:20px; width:20px; background-color: black; border-color:black;"></div></a></li>
+                                   
+                                </ul>
+                            </div>
+                           
+                             
+
+                 
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/scripts.js"></script>
+
+    </body>
+
+</html>
---
+++ wwam-private-beta.php
@@ -0,0 +1,415 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all">
+        <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700" rel="stylesheet">
+    
+    <style>
+
+.signup-for-wwam {
+  width: 758px;
+  height: 22px;
+  margin: 72px 3px 0 0;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.35;
+  letter-spacing: normal;
+  text-align: left;
+  color: #0cb5dd;
+}
+.signup-for-wwam .text-style-1{
+  color: #212121;
+}
+
+.meet-content-text{
+  width: 761px;
+  height: 261px;
+  margin: 0 0 32px;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.3;
+  letter-spacing: normal;
+  text-align: left;
+  color: #212121;
+  text-align: justify;
+}
+
+
+.meet-content-textt {
+  width: 758px;
+  height: 106px;
+  margin: 32px 3px 72px 0;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.3;
+  letter-spacing: normal;
+  text-align: left;
+  color: #212121;
+
+}
+.built-with-love {
+  width: 227px;
+  height: 16px;
+  margin: 0 0 13px;
+  font-family: HelveticaNeue;
+  font-size: 13px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 2.46;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+}
+.built-with-love .text-style-1 {
+  font-weight: bold;
+}
+
+
+.business-blog-jobs{
+  width: 222px;
+  height: 19px;
+  margin: 0 12px 35px 12px;
+  font-family: HelveticaNeue;
+  font-size: 16px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+
+}
+.all-right-reserved{
+    width: 315px;
+  height: 19px;
+  margin: 34px 0 0;
+  font-family: HelveticaNeue;
+  font-size: 16px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+}
+.soon-join {
+  width: 433px;
+  height: 49px;
+  padding-top:24px;
+  margin: 24px 14px 40px 0px;
+  opacity: 0.4;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #111827;
+}
+
+
+.whereweallmeet {
+  width: 358px;
+  height: 53px;
+  margin: 8px 89px 24px 0px;
+  font-family: HelveticaNeue;
+  font-size: 44px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.68;
+  letter-spacing: normal;
+  text-align: left;
+  color: #32b0cf;
+}
+.whereweallmeet .text-style-1 {
+  color: #ca1c67;
+}
+.whereweallmeet .text-style-2 {
+  color: #52bd90;
+}
+.whereweallmeet .text-style-3 {
+  color: #f3b825;
+}
+
+
+.dating-future{
+
+    
+  width: 362px;
+  height: 38px;
+  margin: 0 85px 8px 0px;
+  font-family: HelveticaNeue;
+  font-size: 32px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 0.94;
+  letter-spacing: normal;
+  text-align: left;
+  color: #0cb5dd;
+
+}
+.Signup-to-become-a-WhereWeAllMeetcom-dating-agent-and-get-paid-to-refer-your-social-connections-to {
+  width: 518px;
+  height: 179px;
+  margin: 32px 173px 2px 0;
+  font-family: HelveticaNeue;
+  font-size: 24px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+}
+.Signup-to-become-a-WhereWeAllMeetcom-dating-agent-and-get-paid-to-refer-your-social-connections-to .text-style-1 {
+  font-weight: bold;
+  color: #0cb5dd;
+}
+
+
+.Are-you-a-social-influencer-with-a-large-personal-social-network {
+  width: 409px;
+  height: 230px;
+  margin: 35px 282px 32px 0;
+  font-family: HelveticaNeue;
+  font-size: 52px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.08;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+}     
+
+img.influencer-img {
+  width: 478px;
+  height: 478px;
+  margin: 0 0 0 173px;
+  object-fit: contain;
+}
+
+    
+    .content-text{
+        width: 761px;
+  height: 261px;
+  margin: 0 0 32px;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.3;
+  letter-spacing: normal;
+  text-align: left;
+  color: #212121;
+    }
+       p .Sign-up-for-the-WhereWeAllMeet-private-beta {
+  width: 758px;
+  height: 22px;
+  margin: 72px 3px 0 0;
+  font-family: HelveticaNeue;
+  font-size: 20px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.35;
+  letter-spacing: normal;
+  text-align: left;
+  color: #0cb5dd;
+}
+.Sign-up-for-the-WhereWeAllMeet-private-beta .text-style-1 {
+  color: #212121;
+}
+    </style>
+    </head>
+    <body data-smooth-scroll-offset="77">
+        <div class="nav-container">
+            <div>
+                <nav class="bar bar-toggle">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-6">
+                                <div class="bar__module">
+
+                                        <a href="index.html"> <img class="logo logo-dark" src="img/nav-wwam-logo.png" srcset="img/nav-wwam-logo@2x.png 2x, img/nav-wwam-logo@3x.png 3x"></a>
+                                   
+                                </div>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end">
+                                <div class="bar__module">
+                                 
+
+                                    <!-- <div class="bar__module">
+                                        <a  href="#customise-template"> <span  style="margin-left:-200px;">
+                                        Jobs Blog
+                                    </span> </a>
+                                     
+                                    </div> -->
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="main-container">
+            <section class="text-center unpad--bottom switchable cta cta-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8col-lg-7"> 
+                            <img src="img/webandphone.png" srcset="img/webandphone@2x.png 2x, img/webandphone@3x.png 3x" class="image"> 
+                        </div>
+                        <div class="col-md-4 col-lg-5">
+                            <div class="switchable__text">
+                                <h2 class="dating-future">See the future of dating</h2>
+                                
+                                <span class="whereweallmeet">
+                                    Where<span class="text-style-1">We</span><span class="text-style-2">All</span><span class="text-style-3">Meet</span>
+                                  </span>
+
+                                
+                                <span class="soon-join">
+                                    Where We All Meet will launch soon. Join the wait list and try the beta before it’s publicly available.
+                                  </span>
+                                <div class="feedback" style="margin-top:30px;">
+                                  <form class="row justify-content-center" action="//mrare.us8.list-manage.com/subscribe/post?u=77142ece814d3cff52058a51f&amp;id=f300c9cce8" data-success="Thanks for signing up.  Please check your inbox for a confirmation email." data-error="Please provide your email address.">
+                                    <div class="col-lg-6 col-md-6"> <input class="validate-required validate-email" type="text" name="EMAIL" placeholder="Enter your email" style="width: 270px;
+                                        height: 44px;   padding: 13px 130px 13px 18px;
+                                         border-radius: 6px; border: #eeeff2; background-color: #eeeff2;"> </div>
+                                    <div class="col-lg-6 col-md-6"> <button type="submit" class="btn btn--primary" style=" width: 119px;
+                                        height: 44px;
+                                        padding: 9px 17px 9px 13px;
+                                        border-radius: 6px;
+                                        margin-left:60px;
+                                        background-color: #0cb5dd; font-size: 14px;">Join Waitlist</button> </div>
+                                </form>
+                                </div>
+                            </div>
+
+                          
+                        </div>
+                      
+                    </div>
+                </div>
+            </section>
+      
+           
+        
+            
+            <section class="switchable imagebg" data-overlay="4">
+                <div class="background-image-holder"> <img alt="background" src="img/bgg.png"> </div>
+                <div class="container">
+                    <div class="row justify-content-around">
+                        <div class="col-md-7"> <img src="img/influencer-img.png"
+                            srcset="img/influencer-img@2x.png 2x,
+                                    img/influencer-img@3x.png 3x"
+                            class="influencer-img"></div>
+                        <div class="col-md-5 col-lg-4">
+                            <div class="switchable__text">
+                              
+                                <p class="Are-you-a-social-influencer-with-a-large-personal-social-network" style="color:black;">
+                                    Are you a social 
+                                  influencer with a 
+                                  large personal 
+                                  social network
+
+                                  
+                                </p>
+
+                                
+
+                                <p class="Signup-to-become-a-WhereWeAllMeetcom-dating-agent-and-get-paid-to-refer-your-social-connections-to" style="color:black;" >
+                                    Signup to become a WhereWeAllMeet.com dating agent and get paid to refer your social connections to Premium Members looking for quality connections.</p>
+                                
+                                   <a href="#"> <p class="text-style-1" style="color:#0cb5dd;  font-weight: bold; font-family: HelveticaNeue;
+                                    font-size: 24px;">Signup Now</p></a>
+                                  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+        
+          
+
+
+           
+            <footer class="text-center space--sm footer-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+
+                            <div>
+                                <span class="business-blog-jobs">
+                                    Business
+                                  </span>
+                                  <span class="business-blog-jobs">
+                                    Blog
+                                  </span>
+
+                                  <span class="business-blog-jobs">
+                                    Jobs
+                                  </span>
+                            </div>
+                            
+
+
+                      
+
+                            <div>
+                             
+                                <ul class="social-list list-inline list--hover" >
+                                    <li><a href="#"> <div style="height:20px; width:20px; background-color: black; border-color:black;"></div></a></li>
+                                    <li><a href="#"> <div style="height:20px; width:20px; background-color: black; border-color:black;"></div></a></li>
+                                 
+                                </ul>
+                            </div>
+                           
+                             
+<span class="all-right-reserved">
+     © 2022 WWAM PBLLC. All rights reserved.
+  </span>
+                 
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/scripts.js"></script>
+
+    </body>
+
+</html>
